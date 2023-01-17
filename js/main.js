8a6517d26f4854ecde0c4cf76dd3d62d7e06216a
AdoubleAaron <arododod@gmail.com>
2023-01-17 16:28:34 -0500

    pushing some more code

--- js/main.js
+++ js/main.js
@@ -2,8 +2,6 @@
 
 //Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
 
-
-
 //const stringToNumber = function(str){
 //    return Number(str)
 //}
@@ -13,12 +11,13 @@
 
 
 
+
+
 // You get an array of numbers, return the sum of all of the positives ones.
 
 // Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
 // Note: if there is nothing to sum, the sum is default to 0.
-
 
 
 // function positiveSum(arr) {
@@ -33,12 +32,12 @@
 
 
 
-// You get an array of numbers, return the sum of all of the positives ones.
 
-// Example [1,-4,7,12] => 1 + 7 + 12 = 20
 
-// Note: if there is nothing to sum, the sum is default to 0.
 
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+//Start with function doubleChar(str)
 
 
 //   function doubleChar(str){
@@ -53,17 +52,6 @@
 
 
 
-// while loops 
-
-// let count = 0 
-
-// while(count < 5){
-//   console.log(count)
-//   count ++
-// }
-
-
-
 // convert number to reversed array of digits 
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
 
@@ -72,11 +60,12 @@
 // }
 
 
+
+
+
+
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 // start with let summation = function (num)
-
-
-
 
 
 // let summation = function (num) { 
@@ -91,10 +80,9 @@
 
 
 
+
+
 // Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
-
-
-
 
 // var min = function(list){
 //     return Math.min.apply(null,list);
@@ -103,6 +91,8 @@
 // var max = function(list){
 //     return Math.max.apply(null,list);
 // }
+
+
 
 
 
@@ -121,17 +111,61 @@
 // The correct answer would be 17.
 
 
-function countSheeps(arrayOfSheeps){
-    let num = 0
-    for (let i = 0;i < arrayOfSheeps.length; i++ )
-    if (arrayOfSheeps[i] == true)
-    num++
+// function countSheeps(arrayOfSheeps){
+//     let num = 0
+//     for (let i = 0;i < arrayOfSheeps.length; i++ )
+//     if (arrayOfSheeps[i] == true)
+//     num++
 
-    return num
+//     return num
+// }
+
+
+
+
+
+// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+// Write a code that gives out the total amount for different days(d).
+
+
+// function baseCost(days, rate) {
+//     return days * rate;
+//   }
+  
+//   function discountRate(days) {
+//     if ( days >= 7 ) {
+//       return 50;
+//     }
+//     else if ( days >= 3 ) {
+//       return 20;
+//     }
+//     else {
+//       return 0;
+//     }
+//   }
+  
+//   function rentalCarCost(days) {
+//     return baseCost(days, 40) - discountRate(days);
+//   }
+
+
+
+
+
+
+
+// You ask a small girl,"How old are you?" She always says, "x years old", where x is a random number between 0 and 9.
+
+// Write a program that returns the girl's age (0-9) as an integer.
+
+// Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
+
+
+function getAge(inputString){
+    return parseInt(inputString);
 }
-
-
-
 
 
 
