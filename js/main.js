f1e0db93b054daab853243640caa785a68d57af3
AdoubleAaron <arododod@gmail.com>
2023-01-23 14:18:18 -0500

    some more daily code

--- js/main.js
+++ js/main.js
@@ -190,6 +190,11 @@
 // }
 
 
+
+
+
+
+
 // Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 
 // Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
@@ -200,9 +205,35 @@
 
 
 
-function paperwork(n, m) {
-    if (m < 0 || n < 0) {
-      return 0;
-    }
-    return m * n;
+// function paperwork(n, m) {
+//     if (m < 0 || n < 0) {
+//       return 0;
+//     }
+//     return m * n;
+//   }
+
+
+
+
+
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+
+
+function areYouPlayingBanjo(name) {
+  if (name[0] == 'R' || name[0] == 'r') { 
+    return name + " plays banjo";
   }
+  return name + " does not play banjo";
+}
+
+
+
+
