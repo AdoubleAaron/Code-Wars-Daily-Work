8da7ba63f3a3871dd88bd998c57a1bb550da75bf
AdoubleAaron <arododod@gmail.com>
2023-01-26 17:52:49 -0500

    pushing more codewars

--- js/main.js
+++ js/main.js
@@ -276,9 +276,18 @@
 
 // The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
 
-function cockroachSpeed(s) {
-    //Good Luck!
-    return Math.floor(s*27.777778)
-  }
+// function cockroachSpeed(s) {
+//     //Good Luck!
+//     return Math.floor(s*27.777778)
+//   }
 
 
+
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+
+function solution(str){
+  return str.split('').reverse().join('')
+}
