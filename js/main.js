fab49f2f75205c75dd823fe88f5d1a85ce7e82d8
AdoubleAaron <arododod@gmail.com>
2023-02-04 11:51:43 -0500

    codewars god

--- js/main.js
+++ js/main.js
@@ -325,19 +325,34 @@
 // Note: Empty arrays should return 0.
 
 
-function findAverage(array) {
-  let num = 0 
-  for(let i =0; i < array.length; i++){
-    num += array[i]
+// function findAverage(array) {
+//   let num = 0 
+//   for(let i =0; i < array.length; i++){
+//     num += array[i]
+//   }
+//   if (array.length === 0){
+//     return 0
+//   }else{
+//     return num / array.length
+//   }
+// }
+
+
+
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+
+function squareSum(numbers){
+  var result = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    result += (numbers[i] * numbers[i]);
   }
-  if (array.length === 0){
-    return 0
-  }else{
-    return num / array.length
-  }
+  return result;
 }
-
-
 
 
 
