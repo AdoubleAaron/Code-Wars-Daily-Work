d743e1597c1298f173b858c89a0128edd998571a
AdoubleAaron <arododod@gmail.com>
2023-02-08 08:18:04 -0500

    new code wars

--- js/main.js
+++ js/main.js
@@ -365,3 +365,13 @@
 // String.prototype.isUpperCase = function() {
 //   return this.toUpperCase() === this.toString();
 // }
+
+
+
+
+
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// function repeatStr (n, s) {
+//     return s.repeat(n);
+//   }
