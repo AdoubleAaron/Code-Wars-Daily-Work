b1e2ec72102f88380d20b5b0933f13f1aa82c507
AdoubleAaron <arododod@gmail.com>
2023-02-10 14:28:01 -0500

    adding daily code

--- js/main.js
+++ js/main.js
@@ -394,3 +394,15 @@
       
 //     return result
 //   }
+
+
+
+
+// Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+
+
+// function sumStr(a,b){
+//     let result = +a + +b
+//     return result.toString()
+// }
