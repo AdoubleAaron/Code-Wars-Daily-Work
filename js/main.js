18f5fe9b420ee422636abfc9c73f6d4fd7206dad
AdoubleAaron <arododod@gmail.com>
2023-02-15 16:09:34 -0500

    pushing daily code

--- js/main.js
+++ js/main.js
@@ -410,3 +410,19 @@
 
 
 
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+
+// function shortcut(string){
+//     return string.replace(/[aeiou]/g,'')
+//   }
+
+
+
+
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+}
